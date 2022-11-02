64ebe50ae201ac1c2b63887af8466fa73209937d
Alec Brown <12213854+a1ecbr0wn@users.noreply.github.com>
2022-11-02 22:05:57 +0000

    Automate build of formula (#3)
    * formula template
    * auto generation of formula update pr

--- template/bcd.rb
+++ template/bcd.rb
@@ -1,9 +1,9 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 class Bcd < Formula
-  url "https://github.com/a1ecbr0wn/bcd/archive/refs/tags/v0.2.1.tar.gz"
-  version "v0.2.1"
-  sha256 "d11433113280f7a4c6bd962d3644840671d0d7430101c4fc7c0caab23460447d"
+  url "https://github.com/a1ecbr0wn/bcd/archive/refs/tags/:tap_version.tar.gz"
+  version ":tap_version"
+  sha256 ":tap_sha256"
   desc "Bookmark Change Directory - `bcd` is a way to `cd` to directories that have been bookmarked."
   homepage "http://bcd.noser.net/"
   license "Apache 2.0"
