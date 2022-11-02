436eb005657220e7b8493749d2da4449accc5128
a1ecbr0wn <alec@noser.net>
2022-11-02 12:22:14 +0000

    First build test

--- template/bcd.rb
+++ template/bcd.rb
@@ -1,11 +1,11 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 class Bcd < Formula
-  sha256 "d11433113280f7a4c6bd962d3644840671d0d7430101c4fc7c0caab23460447d"
+  url "https://github.com/a1ecbr0wn/bcd/archive/refs/tags/${tap-version}.tar.gz"
+  version "${tap-version}"
+  sha256 "${tap-sha256}"
   desc "Bookmark Change Directory - `bcd` is a way to `cd` to directories that have been bookmarked."
   homepage "http://bcd.noser.net/"
-  url "https://github.com/a1ecbr0wn/bcd/archive/refs/tags/v0.2.1.tar.gz"
-  version "v0.2.1"
   license "Apache 2.0"
 
   depends_on "rust" => :build
