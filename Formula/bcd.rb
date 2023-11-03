f7376b672befc8bf88f43ca8d419c5ea587966da
Δ ǀ Ξ ȼ <12213854+a1ecbr0wn@users.noreply.github.com>
2023-11-03 12:49:03 +0000

    Update formula to bcd v1.0.9 (#28)
    Co-authored-by: a1ecbr0wn <a1ecbr0wn@users.noreply.github.com>

--- Formula/bcd.rb
+++ Formula/bcd.rb
@@ -1,9 +1,9 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 class Bcd < Formula
-  url "https://github.com/a1ecbr0wn/bcd/archive/refs/tags/v1.0.8.tar.gz"
-  version "v1.0.8"
-  sha256 "b2aa0f2bbddc215243bf8b344fc2e2c819ea0ec4a36de64477e11d8684b8a111"
+  url "https://github.com/a1ecbr0wn/bcd/archive/refs/tags/v1.0.9.tar.gz"
+  version "v1.0.9"
+  sha256 "87db06f4d2869cd07f846f0949e9c5a9d495925b13c09e162ee445bdbc871c31"
   desc "Bookmark Change Directory - `bcd` is a way to `cd` to directories that have been bookmarked."
   homepage "http://bcd.noser.net/"
   license "Apache 2.0"
